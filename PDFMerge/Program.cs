f00ad003ee2e79b6651603c3c97fb121e22dc598
Mateus Viegas <mateuscviegas@gmail.com>
2016-09-09 09:30:49 -0300

    Project created

---
+++ PDFMerge/Program.cs
@@ -0,0 +1,37 @@
+﻿using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDFMerge
+{
+    class Program
+    {
+        static void CopyPages(PdfDocument from, PdfDocument to)
+        {
+            for (int i = 0; i < from.PageCount; i++)
+            {
+                to.AddPage(from.Pages[i]);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var sourcePath1 = Console.ReadLine();
+            var sourcePath2 = Console.ReadLine();
+
+            using (PdfDocument one = PdfReader.Open(@"C:\Users\Mateus\Dropbox\Documentos\SETEMBRO-AMEX-EXTRATO.pdf", PdfDocumentOpenMode.Import))
+            using (PdfDocument two = PdfReader.Open(@"C:\Users\Mateus\Dropbox\Documentos\SETEMBRO-AMEX-BOLETO.pdf", PdfDocumentOpenMode.Import))
+            using (PdfDocument outPdf = new PdfDocument())
+            {                
+                CopyPages(one, outPdf);
+                CopyPages(two, outPdf);
+
+                outPdf.Save(@"C:\Users\Mateus\Desktop\SETEMBRO-AMEX.pdf");
+            }
+        }
+    }
+}
